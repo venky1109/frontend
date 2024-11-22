ff0d102a5a92063a83788945d1be68dfe6525d1e
venky1109 <happyvenkatesh457@gmail.com>
2024-11-22 14:44:11 +0530

    REACT_APP_BASE_URL ADDED for backend

--- src/constants.js
+++ src/constants.js
@@ -11,10 +11,12 @@
 // export const PAYPAL_URL = '/api/config/paypal';
 
 
-export const BASE_URL =
-  process.env.NODE_ENV === 'development'
-    ? 'https://backend-mhpu.onrender.com'  // Local backend server in development
-    : 'https://mkbackend.onrender.com';  // Replace with the correct backend URL for production
+// export const BASE_URL =
+//   process.env.NODE_ENV === 'development'
+//     ? 'https://backend-mhpu.onrender.com'  // Local backend server in development
+//     : 'https://mkbackend.onrender.com';  // Replace with the correct backend URL for production
+
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
