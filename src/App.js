4cee88deeebfffde324834d937428cfb90eb80bb
venky1109 <happyvenkatesh457@gmail.com>
2025-01-29 23:53:54 +0530

    install app banner updated

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import FloatingCartIcon from './components/FloatingCartIcon';
 import { logout } from './slices/authSlice';
 import { ToastContainer } from 'react-toastify';
 import { useWebSocket } from './hooks/useWebSocket';
+import logo from './assets/ManaKiranaLogoWithName.gif';
 
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -77,28 +78,41 @@ const App = () => {
       {/* Custom Install Banner */}
       {showInstallBanner && (
         <div
-          className={`fixed ${
-            window.innerWidth < 640 ? 'top-[6rem]' : 'bottom-4'
-          } left-4 right-4 bg-white shadow-lg rounded-md p-4 flex flex-col sm:flex-row sm:items-center sm:justify-between`}
-        >
-          <p className="text-gray-800 mb-2 sm:mb-0 text-center sm:text-left">
-            Install <span className="font-bold text-green-600">Manakirana</span> for a faster and better experience!
+        className={`fixed ${
+          window.innerWidth < 640 ? "top-[6rem]" : "bottom-4"
+        } left-2 right-2 border border-b-5 border-orange-600 bg-yellow-100 shadow-lg rounded-md p-2 flex items-center sm:justify-between`}
+      >
+        {/* Logo & Text */}
+        <div className="flex items-center space-x-3">
+          <img
+            src={logo}
+            alt="ManaKirana logo"
+            className="w-14 h-auto sm:w-16 lg:w-20 border border-yellow-600 rounded-md"
+          />
+          <p className="text-gray-800 text-sm sm:text-md">
+            Install <span className="font-bold text-green-600">APP</span> for better experience!
           </p>
-          <div className="flex space-x-4 justify-center sm:justify-end">
-            <button
-              onClick={handleInstallClick}
-              className="bg-green-600 text-white px-4 py-2 rounded-md hover:bg-green-700 transition duration-300"
-            >
-              Install Now
-            </button>
-            <button
-              onClick={handleDismissBanner}
-              className="bg-gray-300 text-gray-700 px-4 py-2 rounded-md hover:bg-gray-400 transition duration-300"
-            >
-              Dismiss
-            </button>
-          </div>
         </div>
+      
+        {/* Install & Close Buttons */}
+        <div className="flex items-center space-x-3">
+          <button
+            onClick={handleInstallClick}
+            className="bg-green-600 text-white px-4 py-2 rounded-md hover:bg-green-700 transition duration-300"
+          >
+            Install
+          </button>
+          
+          {/* Close (X) Button */}
+          <button
+            onClick={handleDismissBanner}
+            className="text-gray-600 hover:text-gray-800 text-mb font-semibold focus:outline-none"
+          >
+            ✖
+          </button>
+        </div>
+      </div>
+      
       )}
 
     </div>
